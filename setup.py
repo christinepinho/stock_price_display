13250087f31635b16358bf50287609be8cc757da
Christine Pinho <cpinho@uw.edu>
2018-10-08 20:20:30 -0700

    fixing git url in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version='0.0.1',
     name='stock_price_display',
     description='Displays stock analysis for Google, Microsoft & Capital One',
-    url='https://github.link',
+    url='https://github.com/christinepinho/stock_price_display',
     author='Christine Pinho',
     author_email='christineapinho@gmail.com',
     install_requires=['quandl', 'argparse'],
